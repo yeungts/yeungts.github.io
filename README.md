b670cc40cb2af64a033564c7af9f80cfaa0e5c84
kenye <kenye@KENDRICK>
2022-03-08 16:20:51 -0500

    moved js code into corresponding js file

--- README.md
+++
@@ -1,1 +0,0 @@
-# yeungts.github.io
--- js/main.js
+++ js/main.js
@@ -23,3 +23,43 @@ function reduceVolume() {
     background_audio.volume == 0 ? vol_display.innerHTML = "Vol(MUTED)" : vol_display.innerHTML = "Vol(" + (background_audio.volume*100).toFixed(0) + "%)";
 }
 
+function displayMainWelcome() {
+    $("#main-welcome").css("display", "block");
+    $("#about-me").css("display", "none");
+    $("#resume").css("display", "none");
+    $("#credits").css("display", "none");
+}
+
+function displayAboutMe() {
+    $("#main-welcome").css("display", "none");
+    $("#about-me").css("display", "block");
+    $("#resume").css("display", "none");
+    $("#credits").css("display", "none");
+}
+
+function displayResume() {
+    $("#main-welcome").css("display", "none");
+    $("#about-me").css("display", "none");
+    $("#resume").css("display", "block");
+    $("#credits").css("display", "none");
+    resizeResumeIFrame();
+}
+
+function displayCredits() {
+    $("#main-welcome").css("display", "none");
+    $("#about-me").css("display", "none");
+    $("#resume").css("display", "none");
+    $("#credits").css("display", "block");
+}
+
+function displayMenu() {
+    $("#other-links").css("display") == "none" ? $("#other-links").css("display", "flex") : $("#other-links").css("display", "none");
+}
+
+function resizeResumeIFrame() {
+    if ($(window).width() < 900) {
+        $("#resume-pdf").attr("width", ($(window).width() - 30) + "px");
+        $("#resume-pdf").attr("height", ($(window).width() - 30) * 1.3 + "px");
+    }
+}
+
